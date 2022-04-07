acfcf5a91ec11691ed201d5b02c1e12c0e7baca8
wallacee93 <deshawn.wallace93@gmail.com>
2022-04-07 17:35:46 -0400

    updates

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from "./components/weather/Weather";
+import { Grid } from '@mui/material';
+
+function App() {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <Weather></Weather>
+      </Grid>
+
+    </Grid>
+  );
+}
+
+export default App;
